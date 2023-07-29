e13f31a93fb76b4d9d2b9e973d0d86e1d18b79c6
ikrzywda <krzywdaigor890@gmail.com>
2023-07-29 16:25:17 +0200

    initial-commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,9 @@
 /* Components */
-import { Counter } from './components/Counter/Counter'
 
 export default function IndexPage() {
-  return <Counter />
+  return <></>;
 }
 
 export const metadata = {
-  title: 'Redux Toolkit',
-}
+  title: "Redux Toolkit",
+};
